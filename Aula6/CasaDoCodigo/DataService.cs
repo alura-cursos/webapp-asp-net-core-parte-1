835f2eb17801d29fa506a057dfa6749b4b53ec72
marcelooliveira <mclricardo@gmail.com>
2017-05-09 15:59:48 -0300

    CasaDoCodigo.Aula6

---
+++ Aula6/CasaDoCodigo/DataService.cs
@@ -0,0 +1,48 @@
+﻿using CasaDoCodigo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo
+{
+    public class DataService
+    {
+        private readonly Contexto _contexto;
+        public DataService(Contexto contexto)
+        {
+            this._contexto = contexto;
+        }
+
+        public void InicializaDB()
+        {
+            this._contexto.Database.EnsureCreated();
+            if (this._contexto.Produtos.Count() == 0)
+            {
+                List<Produto> produtos = new List<Produto>
+                {
+                    new Produto(1, "Sleep not found", 59.90m),
+                    new Produto(2, "May the code be with you", 59.90m),
+                    new Produto(3, "Rollback", 59.90m),
+                    new Produto(4, "REST", 69.90m),
+                    new Produto(5, "Design Patterns com Java", 69.90m),
+                    new Produto(6, "Vire o jogo com Spring Framework", 69.90m),
+                    new Produto(7, "Test-Driven Development", 69.90m),
+                    new Produto(8, "iOS: Programe para iPhone e iPad", 69.90m),
+                    new Produto(9, "Desenvolvimento de Jogos para Android", 69.90m)
+                };
+
+                foreach (var produto in produtos)
+                {
+                    this._contexto.Produtos
+                        .Add(produto);
+
+                    this._contexto.ItensPedido
+                        .Add(new ItemPedido(produto, 1));
+                }
+
+                this._contexto.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Aula6/CasaDoCodigo/Models/ItemPedido.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models
+{
+    public class ItemPedido
+    {
+        public int Id { get; private set; }
+        public Produto Produto { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal PrecoUnitario { get; private set; }
+        public decimal Subtotal {
+            get
+            {
+                return Quantidade * PrecoUnitario;
+            }
+        }
+
+        public ItemPedido(int id, Produto produto, 
+            int quantidade) : this(produto, quantidade)
+        {
+            this.Id = id;
+        }
+
+        public ItemPedido(Produto produto,
+            int quantidade)
+        {
+            this.Produto = produto;
+            this.Quantidade = quantidade;
+            this.PrecoUnitario = produto.Preco;
+        }
+    }
+}
