6c7349eb0680df94c9a080591aa09c7fdda67b44
marcelooliveira <mclricardo@gmail.com>
2017-05-09 15:56:46 -0300

    CasaDoCodigo.Aula5

---
+++ Aula5/CasaDoCodigo/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models
+{
+    public class Produto
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public decimal Preco { get; private set; }
+
+        public Produto(int id, string nome, decimal preco)
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Preco = preco;
+        }
+    }
+}
