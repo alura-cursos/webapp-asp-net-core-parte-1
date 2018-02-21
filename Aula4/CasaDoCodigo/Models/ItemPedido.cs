f972c50e60ee59963579a05b78a2a7bbebce5306
marcelooliveira <mclricardo@gmail.com>
2018-02-21 16:01:05 -0300

    Aula 4

---
+++ Aula4/CasaDoCodigo/Models/ItemPedido.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models
+{
+    public class ItemPedido
+    {
+        public int Id { get; private set; }
+        public Produto Produto { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal PrecoUnitario { get; private set; }
+        public decimal Subtotal {
+            get
+            {
+                return Quantidade * PrecoUnitario;
+            }
+        }
+
+        public ItemPedido(int id, Produto produto, 
+            int quantidade)
+        {
+            this.Id = id;
+            this.Produto = produto;
+            this.Quantidade = quantidade;
+            this.PrecoUnitario = produto.Preco;
+        }
+    }
+}
