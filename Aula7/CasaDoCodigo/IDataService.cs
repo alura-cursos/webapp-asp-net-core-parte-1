06cad5bf59d5ed9ecea41ccda83cea5b302b8632
marcelooliveira <mclricardo@gmail.com>
2018-02-21 16:07:39 -0300

    Aula 7

---
+++ Aula7/CasaDoCodigo/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using CasaDoCodigo.Models;
+using System.Collections.Generic;
+
+namespace CasaDoCodigo
+{
+    public interface IDataService
+    {
+        void InicializaDB();
+        List<Produto> GetProdutos();
+        List<ItemPedido> GetItensPedido();
+    }
+}
